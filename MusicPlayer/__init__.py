a41c1bcec54543c7900f1b88dfe5d8a3980e0f58
XLBots / <94062864+piroXpower@users.noreply.github.com>
2023-03-10 12:20:23 +0530

    Update __init__.py

--- MusicPlayer/__init__.py
+++ MusicPlayer/__init__.py
@@ -1,1 +1,16 @@
+import os 
+import logging
+from pyrogram import Client
 
+print("[xl] Starting Deployment!")
+
+CHATS = []
+
+OWNER_ID = config.OWNER_ID
+
+print("[XL] Booting Clients 🎶")
+
+Userbot = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+MusicPlayer = pytgcalls(Userbot) 
+
+print("[XL] Processing To __main__.py") 
