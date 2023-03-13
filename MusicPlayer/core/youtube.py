942217b9a44ed06ae2da74fdfa763ec124108a74
XLBots / <94062864+piroXpower@users.noreply.github.com>
2023-03-13 08:08:06 +0530

    Create youtube.py

---
+++ MusicPlayer/core/youtube.py
@@ -0,0 +1,48 @@
+import asyncio
+import random
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pytgcalls import StreamType
+from youtubesearchpython import VideosSearch
+def convert_seconds(seconds):
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d" % (minutes, seconds)
+
+
+# music player
+def ytsearch(query):
+    try:
+        search = VideosSearch(query, limit=1)
+        for r in search.result()["result"]:
+            ytid = r["id"]
+            if len(r["title"]) > 34:
+                songname = r["title"][:35] + "..."
+            else:
+                songname = r["title"]
+            url = f"https://www.youtube.com/watch?v={ytid}"
+            duration = r["duration"]
+        return [songname, url, duration]
+    except Exception as e:
+        print(e)
+        return 0
+
+
+async def ytdl(link):
+    proc = await asyncio.create_subprocess_exec(
+        "yt-dlp",
+        "-g",
+        "-f",
+        # CHANGE THIS BASED ON WHAT YOU WANT
+        "bestaudio",
+        f"{link}",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await proc.communicate()
+    if stdout:
+        return 1, stdout.decode().split("\n")[0]
+    else:
+        return 0, stderr.decode()
